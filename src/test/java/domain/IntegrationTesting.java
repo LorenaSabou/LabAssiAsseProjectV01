7bc170db0f41d3c7ad8e2d60737e3e6fdbf7b61c
lorenasabou <lorenasabou@yahoo.com>
2019-04-15 16:50:00 +0300

    integration testing top down approach

--- src/test/java/domain/IntegrationTesting.java
+++ src/test/java/domain/IntegrationTesting.java
@@ -55,6 +55,37 @@ public class IntegrationTesting {
         addGrade();
     }
 
+    @Test
+    public void addAssignmentTopDown() {
+        Student student = new Student("10","Lorena", 936);
+        studentRepository.save(student);
+
+
+        Tema temaToAdd = new Tema("5","lorena",4,1);
+        temaRepository.save(temaToAdd);
+
+        assert temaRepository.findOne("5").equals(temaToAdd) &&
+                studentRepository.findOne("10").equals(student);;
+    }
+
+    @Test
+    public void addGradeTopDown() {
+        Student student = new Student("10","Lorena", 936);
+        studentRepository.save(student);
+
+
+        Tema temaToAdd = new Tema("5","lorena",4,1);
+        temaRepository.save(temaToAdd);
+
+        Nota gradeToAdd = new Nota(new Pair<>("10","5"),10,6,"foarte bine");
+        notaRepository.save(gradeToAdd);
+        Pair pairToSearch = new Pair("10","5");
+
+
+        assert temaRepository.findOne("5").equals(temaToAdd) &&
+                studentRepository.findOne("10").equals(student) &&
+                notaRepository.findOne(pairToSearch).equals(gradeToAdd);
+    }
 
 }
 
